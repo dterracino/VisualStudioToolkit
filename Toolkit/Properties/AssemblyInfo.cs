3ec2ab2e1dc2bf1baccad3c3e6ed4ad6061a9e67
Thijs Tijsma <thijs@thesolutionengineers.com>
2016-02-15 17:36:18 +0100

    Added extension project.

---
+++ Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Company
+
+[assembly: AssemblyCompany("The Solution Engineers")]
+
+// Product
+
+[assembly: AssemblyProduct("The Solution Engineer Toolkit for Visual Studio")]
+[assembly: AssemblyVersion("1.0")]
+
+// COM
+
+[assembly: ComVisible(false)]
