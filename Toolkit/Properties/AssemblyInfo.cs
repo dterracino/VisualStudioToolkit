24932ca707194c3890891b70927d0a277e614ce4
Thijs Tijsma <thijs@thesolutionengineers.com>
2016-04-21 20:21:50 +0200

    Updated version.

--- Toolkit/Properties/AssemblyInfo.cs
+++ Toolkit/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // Product
 
 [assembly: AssemblyProduct("The Solution Engineer Toolkit for Visual Studio")]
-[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyVersion("1.1")]
 
 // COM
 
